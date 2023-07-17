3a9f5cb473526b421d55b94a4fbc984bdf0c02f5
Nannaan <nonodera65@gmail.com>
2023-07-17 14:12:21 +0900

    一覧表示機能の実装

---
+++ db/migrate/20230717013813_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string   :task_name
+      t.datetime :deadline
+      t.integer  :required_time
+      t.integer  :time_unit
+      t.timestamps
+    end
+  end
+end
