7c3a9aea6f0768e926bd8a476104e0baf7a6f5f5
Nannaan <nonodera65@gmail.com>
2023-07-17 17:39:11 +0900

    タスク編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  resources :tasks, only: [:index, :new, :create, :destroy]
+  resources :tasks, only: [:index, :new, :create, :destroy, :edit, :update]
   post '/tasks/new', to: 'tasks#create'
 end
