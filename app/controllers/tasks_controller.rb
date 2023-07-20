2ae6251dfce90f8099f7f653580cc29e621c4df0
Nannaan <nonodera65@gmail.com>
2023-07-20 15:23:41 +0900

    一覧表示画面の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task_name, :deadline, :required_time)
+    params.permit(:task_name, :deadline, :required_time)
   end
 
 end
